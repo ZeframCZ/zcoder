b0995b8f39fb31326bdc8fa951367342968a4903
ZeframCZ <36083916+ZeframCZ@users.noreply.github.com>
2018-02-03 10:07:49 +0100

    Add files via upload

--- zcoder.py
+++ zcoder.py
@@ -32,7 +32,7 @@ font = ImageFont.load_default()
 #--------------------SET UP PLACEHOLDERS--------------------
 update = True
 timer = 10
-text_rotation = 180
+text_rotation = 280
 #--------------------DEFINE DRAW FUNCTION--------------------
 def draw_rotated_text(image, text, position, angle, font, fill=(255,255,255)):
     # Get rendered font width and height.
@@ -56,12 +56,12 @@ while(True):#repeat
         sens_accX, sens_accY, sens_accZ = motion.accelerometer()
         #--------------------DRAW STUFF FROM SENSORS--------------------
         draw_rotated_text(disp.buffer, 'Teplota: '+str(sens_temperature), (10, 10), text_rotation, font, fill=(255,255,255))
-        draw_rotated_text(disp.buffer, 'Tlak: '+str(sens_pressure), (30, 10), text_rotation, font, fill=(255,255,255))
-        draw_rotated_text(disp.buffer, 'Svetlo: '+str(sens_light), (50, 10), text_rotation, font, fill=(255,255,255))
-        draw_rotated_text(disp.buffer, 'Akcelerace', (70, 10), text_rotation, font, fill=(255, 255, 255))
-        draw_rotated_text(disp.buffer, 'X: ' + str(sens_accX), (90, 10), text_rotation, font, fill=(255, 255, 255))
-        draw_rotated_text(disp.buffer, 'Y: ' + str(sens_accY), (110, 10), text_rotation, font, fill=(255, 255, 255))
-        draw_rotated_text(disp.buffer, 'Z: ' + str(sens_accZ), (130, 10), text_rotation, font, fill=(255, 255, 255))
+        draw_rotated_text(disp.buffer, 'Tlak: '+str(sens_pressure), (10, 30), text_rotation, font, fill=(255,255,255))
+        draw_rotated_text(disp.buffer, 'Svetlo: '+str(sens_light), (10, 50), text_rotation, font, fill=(255,255,255))
+        draw_rotated_text(disp.buffer, 'Akcelerace', (10, 70), text_rotation, font, fill=(255, 255, 255))
+        draw_rotated_text(disp.buffer, 'X: ' + str(sens_accX), (10, 90), text_rotation, font, fill=(255, 255, 255))
+        draw_rotated_text(disp.buffer, 'Y: ' + str(sens_accY), (10, 110), text_rotation, font, fill=(255, 255, 255))
+        draw_rotated_text(disp.buffer, 'Z: ' + str(sens_accZ), (10, 130), text_rotation, font, fill=(255, 255, 255))
         disp.display()
         update = False
     ##--------------------DELAY SCREEN UPDATE--------------------
