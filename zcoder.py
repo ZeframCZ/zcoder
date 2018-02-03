630370eb8667ffbdfbbcfa5ea30ca7e5850589e9
ZeframCZ <36083916+ZeframCZ@users.noreply.github.com>
2018-02-03 21:12:10 +0100

    Add files via upload

--- zcoder.py
+++ zcoder.py
@@ -60,7 +60,8 @@ while(True):#repeat
         sens_light = light.light()
         sens_accX, sens_accY, sens_accZ = motion.accelerometer()
         #cpu_temp = (subprocess.check_output(["/opt/vc/bin/vcgencmd", "measure_temp"]))
-        cpu_temp = (subprocess.check_output(["echo $((`cat /sys/class/thermal/thermal_zone0/temp`/1000))"]))
+        #cpu_temp = (subprocess.check_output(["echo $((`cat /sys/class/thermal/thermal_zone0/temp`/1000))"]))         cmd = "echo '{}' | iconv ...".format(text)
+        cmd = "echo $((`cat /sys/class/thermal/thermal_zone0/temp`/1000))".format(text)
         #--------------------DRAW STUFF FROM SENSORS--------------------240x320
         #get width of texts
 
