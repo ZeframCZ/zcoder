4cedd68fe93cb98550e3edfa80c8befd3f504a62
ZeframCZ <36083916+ZeframCZ@users.noreply.github.com>
2018-03-15 21:56:08 +0100

    Add files via upload

--- zc.py
+++ zc.py
@@ -36,7 +36,7 @@ disp.begin()
 disp.clear((0, 0, 0))
 draw = disp.draw()
 #font = ImageFont.load_default()
-font = ImageFont.truetype('arial.ttf', 8, encoding="unic")
+font = ImageFont.truetype("/usr/share/fonts/truetype/lato/Lato-Medium.ttf", 8, encoding="unic")
 #font_large = ImageFont.truetype(12)ImageFont.truetype("arial.ttf", fontsize)
 #--------------------SET UP PLACEHOLDERS--------------------
 update = True
