85256d026b5bb81f65defc69d7f7a10f96ffca67
ZeframCZ <36083916+ZeframCZ@users.noreply.github.com>
2018-03-16 19:33:40 +0100

    Add files via upload

--- zc.py
+++ zc.py
@@ -162,5 +162,5 @@ while(True):#repeat
     #draw line,date and time
     draw.line((0, 25, 240, 25), fill=(255,255,255))
     w1, h1 = draw.textsize(str(date))
-    draw_rotated_text(disp.buffer, str(date), (120-w1, 0), text_rotation, font_small,fill=(255, 255, 255))
+    draw_rotated_text(disp.buffer, str(date), (140-w1, 0), text_rotation, font_small,fill=(255, 255, 255))
     disp.display()
