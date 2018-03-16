3cddf0a52d803107de9f69aa8a4dc397ba04a965
ZeframCZ <36083916+ZeframCZ@users.noreply.github.com>
2018-03-16 17:32:23 +0100

    Add files via upload

--- zc.py
+++ zc.py
@@ -44,7 +44,7 @@ update = True
 timer = 10
 text_rotation = 180
 screen_width = 230
-dist_sens = False#switch betwen screens
+dist_sens = True#switch between screens
 
 #--------------------DEFINE DRAW FUNCTION--------------------
 def draw_rotated_text(image, text, position, angle, font, fill=(255,255,255)):
@@ -60,70 +60,95 @@ def draw_rotated_text(image, text, position, angle, font, fill=(255,255,255)):
     rotated = textimage.rotate(angle, expand=1)
     # Paste the text into the image, using it as a mask for transparency.
     image.paste(rotated, position, rotated)
+
+
 while(True):#repeat
-            if dist_sens = False:
-                #--------------------GET STUFF FROM SENSORS--------------------
-                sens_temperature = weather.temperature()
-                sens_pressure = weather.pressure()
-                sens_light = light.light()
-                sens_accX, sens_accY, sens_accZ = motion.accelerometer()
-                sens_heading = motion.heading()
-                sens_height = 600
-                sens_distance = 0.0
+    #--------------------DISTANCE SENSOR DATA SCREEN--------------------
+    if (dist_sens):
+        GPIO.output(TRIG, False)
+        dsens_status = "Waitng for sensor to settle"
+        time.sleep(0.1)
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
+        while GPIO.input(ECHO) == 0:
+            pulse_start = time.time()
+        while GPIO.input(ECHO) == 1:
+            pulse_end = time.time()
+        pulse_duration = pulse_end - pulse_start
+        distance = pulse_duration * 17150
+        distance = round(distance, 2)
+        if distance > 2 and distance < 400:  # Check whether the distance is within range
+            dsens_status = "Done"
+            print"Distance:", distance - 0.5, "cm"  # Print distance with 0.5 cm calibration
+        else:
+            dsens_status = "Out of sensor range"
 
-                #--------------------DRAW STUFF FROM SENSORS--------------------240x320
+        draw_rotated_text(disp.buffer, "Sensor status: " + str(dsens_status), (240, 320), text_rotation, font, fill=(255, 255, 255))
+        draw_rotated_text(disp.buffer,"Distance: " + str(distance - 0.5) + "cm", (240, 300), text_rotation, font, fill=(255, 255, 255))
+    #--------------------MAIN SENSOR DATA--------------------
+    else:
+        #--------------------GET STUFF FROM SENSORS--------------------
+        sens_temperature = weather.temperature()
+        sens_pressure = weather.pressure()
+        sens_light = light.light()
+        sens_accX, sens_accY, sens_accZ = motion.accelerometer()
+        sens_heading = motion.heading()
+        sens_height = 600
+        sens_distance = 0.0
 
-                w1, h1 = draw.textsize("ZCoder 2.0")
-                w2, h2 = draw.textsize("" + str(math.floor(sens_temperature)))
-                w3, h3 = draw.textsize("" + str(math.floor(sens_pressure)))
-                w4, h4 = draw.textsize("" + str(sens_light))
-                w5, h5 = draw.textsize("")
-                w6, h6 = draw.textsize("X: " + str(math.floor(sens_accX)))
-                w7, h7 = draw.textsize("Y: " + str(math.floor(sens_accY)))
-                w8, h8 = draw.textsize("Z: " + str(math.floor(sens_accZ)))
-                w10, h10 = draw.textsize("Kompas: " + str(sens_heading))
-                w11, h11 = draw.textsize("" + str(sens_height))
-                #w11, h11 = draw.textsize("Vzdalenost: " + str(sens_distance))
+        #--------------------DRAW STUFF FROM SENSORS--------------------240x320
 
-                #draw_rotated_text(disp.buffer, "ZCoder 2.0", (screen_width - w1, 310), text_rotation, font,fill=(255, 255, 255))
+        w1, h1 = draw.textsize("ZCoder 2.0")
+        w2, h2 = draw.textsize("" + str(math.floor(sens_temperature)))
+        w3, h3 = draw.textsize("" + str(math.floor(sens_pressure)))
+        w4, h4 = draw.textsize("" + str(sens_light))
+        w5, h5 = draw.textsize("")
+        w6, h6 = draw.textsize("X: " + str(math.floor(sens_accX)))
+        w7, h7 = draw.textsize("Y: " + str(math.floor(sens_accY)))
+        w8, h8 = draw.textsize("Z: " + str(math.floor(sens_accZ)))
+        w10, h10 = draw.textsize("Kompas: " + str(sens_heading))
+        w11, h11 = draw.textsize("" + str(sens_height))
+        #w11, h11 = draw.textsize("Vzdalenost: " + str(sens_distance))
 
-                #Left up temperature
-                draw.rectangle((217, 310, 132, 225), outline=(255, 255, 255), fill=(0, 120, 255))
-                draw_rotated_text(disp.buffer,str(math.floor(sens_temperature)), (174-w2,290-h2),text_rotation, font, fill=(255, 255, 255))
-                #right up pressure
-                draw.rectangle((108, 310,23, 225), outline=(255, 255, 255), fill=(0, 120, 255))
-                draw_rotated_text(disp.buffer, str(math.floor(sens_pressure)), (67-w3, 290-h3), text_rotation, font,fill=(255, 255, 255))
+        #draw_rotated_text(disp.buffer, "ZCoder 2.0", (screen_width - w1, 310), text_rotation, font,fill=(255, 255, 255))
 
-                #Left mid light
-                draw.rectangle((217, 215,  132, 130), outline=(255, 255, 255), fill=(0, 120, 255))
-                draw_rotated_text(disp.buffer, str(math.floor(sens_light)), (174-w4, 200-h4), text_rotation, font,fill=(255, 255, 255))
-                #right mid height
-                draw.rectangle((108, 215, 23, 130), outline=(255, 255, 255), fill=(0, 120, 255))
-                draw_rotated_text(disp.buffer, str(math.floor(sens_height)), (67-w11, 200-h11), text_rotation, font,fill=(255, 255, 255))
+        #Left up temperature
+        draw.rectangle((217, 310, 132, 225), outline=(255, 255, 255), fill=(0, 120, 255))
+        draw_rotated_text(disp.buffer,str(math.floor(sens_temperature)), (174-w2,290-h2),text_rotation, font, fill=(255, 255, 255))
+        #right up pressure
+        draw.rectangle((108, 310,23, 225), outline=(255, 255, 255), fill=(0, 120, 255))
+        draw_rotated_text(disp.buffer, str(math.floor(sens_pressure)), (67-w3, 290-h3), text_rotation, font,fill=(255, 255, 255))
 
-                # Left down acc
-                #draw_rotated_text(disp.buffer, str(math.floor(sens_light)), (132-10, 35-10), text_rotation, font,fill=(255, 255, 255))
-                # right down compass
-                draw.ellipse((23, 35, 108, 120), outline=(255, 255, 255), fill=(0, 120, 255))
-                xx = (math.cos(sens_heading * math.pi / 180) * 40) + 67
-                yy = (math.sin(sens_heading * math.pi / 180) * 40) + 79
-                draw.line((67, 79, xx, yy), fill=(0, 255, 0))
-                xx = (math.cos(-(sens_heading * math.pi / 180)) * 40) + 67
-                yy = (math.sin(-(sens_heading * math.pi / 180)) * 40) + 79
-                draw.line((67, 79, xx, yy), fill=(255, 0, 0))
+        #Left mid light
+        draw.rectangle((217, 215,  132, 130), outline=(255, 255, 255), fill=(0, 120, 255))
+        draw_rotated_text(disp.buffer, str(math.floor(sens_light)), (174-w4, 200-h4), text_rotation, font,fill=(255, 255, 255))
+        #right mid height
+        draw.rectangle((108, 215, 23, 130), outline=(255, 255, 255), fill=(0, 120, 255))
+        draw_rotated_text(disp.buffer, str(math.floor(sens_height)), (67-w11, 200-h11), text_rotation, font,fill=(255, 255, 255))
+
+        # Left down acc
+        #draw_rotated_text(disp.buffer, str(math.floor(sens_light)), (132-10, 35-10), text_rotation, font,fill=(255, 255, 255))
+        # right down compass
+        draw.ellipse((23, 35, 108, 120), outline=(255, 255, 255), fill=(0, 120, 255))
+        xx = (math.cos(sens_heading * math.pi / 180) * 40) + 67
+        yy = (math.sin(sens_heading * math.pi / 180) * 40) + 79
+        draw.line((67, 79, xx, yy), fill=(0, 255, 0))
+        xx = (math.cos(-(sens_heading * math.pi / 180)) * 40) + 67
+        yy = (math.sin(-(sens_heading * math.pi / 180)) * 40) + 79
+        draw.line((67, 79, xx, yy), fill=(255, 0, 0))
 
 
-                if (update):
-                    update = False
-                if (timer > 1):
-                    timer -= 1
-                if (timer < 2):
-                    disp.clear((0, 0, 0))
-                    update = True
-                    timer = 5  # delay time
-                date = datetime.now()
-                #draw line,date and time
-                draw.line((0, 10, 240, 10), fill=(255,255,255))
-                draw_rotated_text(disp.buffer, str(date), (50, 0), text_rotation, font_small,fill=(255, 255, 255))
-
-                disp.display()
+    if (update):
+        update = False
+    if (timer > 1):
+        timer -= 1
+    if (timer < 2):
+        disp.clear((0, 0, 0))
+        update = True
+        timer = 5  # delay time
+    date = datetime.now()
+    #draw line,date and time
+    draw.line((0, 10, 240, 10), fill=(255,255,255))
+    draw_rotated_text(disp.buffer, str(date), (50, 0), text_rotation, font_small,fill=(255, 255, 255))
+    disp.display()
