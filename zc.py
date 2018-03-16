f57fb7ee5eb888be6f623e375d02acbd8244b9f5
ZeframCZ <36083916+ZeframCZ@users.noreply.github.com>
2018-03-16 18:16:56 +0100

    Add files via upload

--- zc.py
+++ zc.py
@@ -98,11 +98,11 @@ while(True):#repeat
         distance = round(distance, 2)
         if distance > 2 and distance < 400:  # Check whether the distance is within range
             dsens_status = "Done"
-            draw_rotated_text(disp.buffer, "Distance: " + str(distance - 0.5) + "cm", (150, 200), text_rotation, font, fill=(255, 255, 255))
+            draw_rotated_text(disp.buffer, "Distance: " + str(distance - 0.5) + "cm", (240, 200), text_rotation, font, fill=(255, 255, 255))
         else:
             dsens_status = "Out of sensor range"
 
-        draw_rotated_text(disp.buffer, "Sensor status: " + str(dsens_status), (150, 200), text_rotation, font, fill=(255, 255, 255))
+        #draw_rotated_text(disp.buffer, "Sensor status: " + str(dsens_status), (150, 200), text_rotation, font, fill=(255, 255, 255))
     #--------------------MAIN SENSOR DATA--------------------
     if (dist_sens == False):
         #--------------------GET STUFF FROM SENSORS--------------------
