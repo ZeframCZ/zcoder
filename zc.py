ac6c787702e04b27975b303f381027680686ebdd
ZeframCZ <36083916+ZeframCZ@users.noreply.github.com>
2018-03-17 20:46:07 +0100

    Add files via upload

--- zc.py
+++ zc.py
@@ -137,20 +137,20 @@ while(True):#repeat
         #Left up temperature
         w, h = draw.textsize(str(int(sens_temperature)) + " C")
         draw.rectangle((217, 310, 132, 225), outline=(255, 255, 255), fill=(0, 120, 255))
-        draw_rotated_text(disp.buffer,str(int(sens_temperature))+" C", (174-w,271-h),text_rotation, font, fill=(255, 255, 255))
+        draw_rotated_text(disp.buffer,str(int(sens_temperature))+" C", (174-w,221-h),text_rotation, font, fill=(255, 255, 255))
         #right up pressure
         w, h = draw.textsize(str(int(sens_pressure/100))+"hPa")
         draw.rectangle((108, 310,23, 225), outline=(255, 255, 255), fill=(0, 120, 255))
-        draw_rotated_text(disp.buffer, str(int(sens_pressure/100))+"hPa", (63-w, 271-h), text_rotation, font_mid,fill=(255, 255, 255))
+        draw_rotated_text(disp.buffer, str(int(sens_pressure/100))+"hPa", (63-w, 221-h), text_rotation, font_mid,fill=(255, 255, 255))#271
 
         #Left mid light
         w, h = draw.textsize(str(int(sens_light))+"lx")
         draw.rectangle((217, 215,  132, 130), outline=(255, 255, 255), fill=(light.rgb()))
-        draw_rotated_text(disp.buffer, str(int(sens_light))+"lx", (174-w, 171-h), text_rotation, font,fill=(255, 255, 255))
+        draw_rotated_text(disp.buffer, str(int(sens_light))+"lx", (174-w, 150-h), text_rotation, font,fill=(255, 255, 255))#171
         #right mid height
         w, h = draw.textsize(str(int(sens_height))+"m")
         draw.rectangle((108, 215, 23, 130), outline=(255, 255, 255), fill=(0, 120, 255))
-        draw_rotated_text(disp.buffer, (str(int(sens_height)))+"m", (63-w, 171-h), text_rotation, font_mid,fill=(255, 255, 255))
+        draw_rotated_text(disp.buffer, (str(int(sens_height)))+"m", (63-w, 150-h), text_rotation, font_mid,fill=(255, 255, 255))
 
         # Left down acc
         x1 = 174
