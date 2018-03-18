0001c9e220eaee49995432ee7e7e67de3ba297d4
ZeframCZ <36083916+ZeframCZ@users.noreply.github.com>
2018-03-18 18:12:11 +0100

    Add files via upload

--- zsw.py
+++ zsw.py
@@ -1,11 +1,2 @@
-import RPi.GPIO as GPIO
-import time
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(16,GPIO.IN,pull_up_down=GPIO.PUD_UP)
-
-while True:
-    input_state = GPIO.input(16)
-    if input_state == False:
-        print("Button ON")
-        time.sleep(0.2)
+from envirophat import analog
+print(analog.read_all())
