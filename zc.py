7c7ccf5059e6feb700e711f00c009601d0d4be8d
ZeframCZ <36083916+ZeframCZ@users.noreply.github.com>
2018-03-18 21:31:27 +0100

    Add files via upload

--- zc.py
+++ zc.py
@@ -224,11 +224,11 @@ while(True):#repeat
         #disp.clear((0, 0, 0))
         update = True
         timer = 5  # delay time
-        date = datetime.now()
+        hour, minute, second, msecond, tzinf = datetime.time()
 
     #draw line,date and time
     draw.line((0, 25, 240, 25), fill=(255,255,255))
-    w1, h1 = draw.textsize(str(datetime.hour)+":"+str(datetime.minute))
-    draw_rotated_text(disp.buffer, str(datetime.hour)+":"+str(datetime.minute), (120 - w1, 8), text_rotation, font_small, fill=(255, 255, 255))
+    w1, h1 = draw.textsize(str(hour)+":"+str(minute)+":"+str(second))
+    draw_rotated_text(disp.buffer, str(hour)+":"+str(minute)+":"+str(second), (120 - w1, 8), text_rotation, font_small, fill=(255, 255, 255))
 
     disp.display()
