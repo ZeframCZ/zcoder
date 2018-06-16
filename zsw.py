73a32674c8309351671bc0d24840c0a955553d7f
ZeframCZ <36083916+ZeframCZ@users.noreply.github.com>
2018-06-16 11:12:04 +0200

    Update zsw.py

--- zsw.py
+++ zsw.py
@@ -1,3 +1,11 @@
-from envirophat import analog
-while True:
-    print(analog.read_all())
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+GPIO.setup(16,GPIO.OUT)
+print "LED on"
+GPIO.output(16,GPIO.HIGH)
+time.sleep(1000)
+print "LED off"
+GPIO.output(16,GPIO.LOW)
